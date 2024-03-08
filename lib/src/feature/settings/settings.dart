861ab2238abb5e09aa6a6ebf386daafec3a56f7b
l.vetter <l.vetter@sgservice.de>
2024-03-08 09:45:43 +0100

    basic example for settings page

--- lib/src/feature/settings/settings.dart
+++ lib/src/feature/settings/settings.dart
@@ -1,10 +1,26 @@
-import 'package:flutter/widgets.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends HookConsumerWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    var index = useState(0);
+    return ScaffoldPage.scrollable(
+      children: [
+        const Text('Some Settings here...'),
+        for (var i = 0; i < 10; i++)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RadioButton(
+              checked: index.value == i,
+              onChanged: (value) => index.value = i,
+              content: Text('Option $i'),
+            ),
+          ),
+      ],
+    );
   }
 }
